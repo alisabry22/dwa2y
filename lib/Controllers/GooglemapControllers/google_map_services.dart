4e08cbbeb93be79822f4678f63715111b6df5af3
alisabry22 <alysabry722@gmail.com>
2022-12-18 15:25:10 +0200

    done

--- lib/Controllers/GooglemapControllers/google_map_services.dart
+++ lib/Controllers/GooglemapControllers/google_map_services.dart
@@ -8,7 +8,7 @@ import 'package:dwa2y/Controllers/LocationController/location_controller.dart';
 =======
 >>>>>>> 8e4e7de5e95a49a25ad084da6a64a20ce88762d6
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_google_places/flutter_google_places.dart';
 import 'package:geocoding/geocoding.dart';
 import 'package:get/get.dart';
@@ -17,7 +17,6 @@ import 'package:google_maps_flutter/google_maps_flutter.dart';
 import '../../Models/user_model.dart';
 
 class GoogleMapServicers extends GetxController {
-<<<<<<< HEAD
   Rx<TextEditingController> searchPlace=TextEditingController().obs;
   RxDouble latitude=0.0.obs;
   RxDouble longitude=0.0.obs;
@@ -30,14 +29,6 @@ class GoogleMapServicers extends GetxController {
         zoom: 14.4746).obs;
   RxList <Marker> markers=[  Marker(markerId: MarkerId("1"), position: LatLng(20.42796133580664,75.885749655962),infoWindow: InfoWindow(title: "My Position"),),
         ].obs;
-=======
- late GoogleMapController controller ;
-  Rx<TextEditingController> searchPlace=TextEditingController().obs;
-   CameraPosition cameraPosition=CameraPosition(target: LatLng(20.42796133580664,75.885749655962),zoom: 11);
-   RxDouble lat=0.0.obs;
-   RxDouble long=0.0.obs;
-   RxList<Marker> markers=RxList.empty();
->>>>>>> 8e4e7de5e95a49a25ad084da6a64a20ce88762d6
 
   Rx<UserModel> currentUserData = UserModel(
           username: "",
@@ -56,7 +47,6 @@ class GoogleMapServicers extends GetxController {
   @override
   void onInit()async {
     currentUserData.value=await getInitialData();
-<<<<<<< HEAD
     currentUserData.bindStream(getCrruntUserData());
     // lat.value=double.parse(currentUserData.value.lat) ;
     // long.value=double.parse(currentUserData.value.long) ;
@@ -75,21 +65,6 @@ markers.refresh();
 
 
 
-=======
-    lat.value=double.parse(currentUserData.value.lat);
-    long.value=double.parse(currentUserData.value.long);
-    
-    cameraPosition=CameraPosition(target: LatLng(lat.value,long.value),zoom: 11);
-    markers.add(
-       Marker(markerId: const MarkerId("1"),
-            position: LatLng(lat.value,long.value),
-            infoWindow:const InfoWindow(title: "My Position"),
-          ),
-         
-    );
-     markers.refresh();
-      
->>>>>>> 8e4e7de5e95a49a25ad084da6a64a20ce88762d6
 
 
     super.onInit();
@@ -116,10 +91,6 @@ markers.refresh();
       return UserModel.fromDocumentSnapshot(event);
     });
   }
-<<<<<<< HEAD
-=======
-  void handleSearchInMap(){
-    
->>>>>>> 8e4e7de5e95a49a25ad084da6a64a20ce88762d6
-
 }
+
+
